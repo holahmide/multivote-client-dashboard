2446c24618c7511ccc633b7b6df2833723d9431f
Adeniyi Olamide <olami02bj@gmail.com>
2022-05-25 13:46:10 +0100

    few changes

--- src/components/Dashboard/sessions/session/nominees.vue
+++ src/components/Dashboard/sessions/session/nominees.vue
@@ -35,9 +35,9 @@
               <div class="text-subtitle-1">
                 {{ nominee.department ? nominee.department : "Not specified" }}
               </div>
-              <!-- <div class="text-primary text-subtitle-2 q-mt-sm">
+              <div class="text-white text-subtitle-2 q-mt-sm">
                 {{ nominee.votes }} vote(s)
-              </div> -->
+              </div>
               <div class="q-mt-sm">
                 <div>
                   <q-btn
